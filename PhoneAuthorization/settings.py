fa7abd930f25d71191f0013813f75ef4607d5783
Reptil0id <Maksim348>
2023-08-20 17:22:45 +0400

    add settings db

--- PhoneAuthorization/settings.py
+++ PhoneAuthorization/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'PhoneAuthorization.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'PhoneAuthorization',
-#         'USER': 'Admin',
-#         'PASSWORD': 'Maksim348',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "reptiloid348.mysql",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbtest_7zh5',
+        'USER': 'admin',
+        'PASSWORD': 'qTVBWABpbvujwbowNfc6qAi2cq98sMhc',
+        'HOST': 'dpg-cjh140b6fquc73br7gi0-a',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "reptiloid348.mysql",
+#     }
+# }
 
 
 # Password validation
